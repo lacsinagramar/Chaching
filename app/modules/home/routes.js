a268a9b1abe2a8770bedd69494ffbf2c70cc97f0
joshuaraemacaya <macaya737@gmail.com>
2018-11-11 03:21:32 +0800

    algorithm

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -8,16 +8,24 @@ var anakSession = {};
 
 router.get('/magulang', function(req, res){
     console.log(magulangSession)
-    res.render('home/views/magulangIndex')
+    queryString = `SELECT * from tbl_goal JOIN tbl_anak ON tbl_anak.intAnakId = tbl_goal.intAnakId
+    WHERE tbl_goal.intMagulangId = ? AND tbl_goal.boolStatus = 0`
+    db.query(queryString,[magulangSession.magulang.intUserId],(err,results1,fields)=>{
+        if(err) throw err;
+        console.log(magulangSession.magulang.intMagulangId)
+        console.log(results1)
+        res.render('home/views/magulangIndex',{goals:results1})
+    })
+    
+
 })
 router.get('/anakIndex', function(req, res){
     res.render('home/views/anakIndex')
 })
 router.get('/', function(req, res){
     res.render('home/views/index')
-    magulangSession = "";
 })
-router.get('/anak', function(req, res){
+router.get('/login/anak', function(req, res){
     res.render('home/views/anak')
     magulangSession = "";
 })
@@ -25,15 +33,54 @@ router.post('/login', function(req, res){
     var queryString = `SELECT * FROM tbl_magulang WHERE strUserName = ? AND strPassword =?`
     db.query(queryString,[req.body.username,req.body.userpassword],(err,results,fields)=>{
         if(err) throw err;
+        console.log(results)
         if(results.length > 0){
             res.send({valid:true, results:results})
+            magulangSession.magulang = results[0];
+            console.log(magulangSession.magulang)
+            console.log(magulangSession)
         }
         else{
             res.send({valid:false})
         }
-        magulangSession.magulang = results[0];
-        console.log(magulangSession.magulang)
-        console.log(magulangSession)
+        
+    })
+})
+router.post('/updateGoal',function(req,res){
+    var queryString = `UPDATE tbl_goal SET boolStatus = 1 WHERE intGoalId = ${req.body.id}`
+    db.query(queryString,(err,results,fields)=>{
+        if(err) throw err;
+        var d = [5,10,20,50,100,500,1000]
+        var queryString1 = `SELECT * FROM tbl_goal WHERE intGoalId = ${req.body.id}`
+        db.query(queryString1,(err,results1,fields)=>{
+            var goalPrice = results1[0].dblGoal
+            var d1 = [];
+            console.log("Goal "+goalPrice)
+            for(i=0;i<d.length;i++){
+                if(goalPrice <= d[i]){
+                    d1.push(d[i-3])
+                    d1.push(d[i-2])
+                    d1.push(d[i-1])
+                }
+            }
+            console.log("denominations: "+d1)
+            var r1 = goalPrice/6
+            var r2 = r1*2
+            var r3 = r1*3
+            var ctr = []
+            ctr.push(Math.ceil(r1/d1[0]))
+            ctr.push(Math.ceil(r2/d1[1]))
+            ctr.push(Math.ceil(r3/d1[2]))
+            for(var i = 0; i<ctr.length; i++){
+                var iinsert = {denomination:d1[i],count:ctr[i]}
+                var queryString = `INSERT INTO tbl_level(intLevel,jsonLevel,intGoalId)
+                VALUES(${i+1},'${JSON.stringify(iinsert)}',${req.body.id})`
+                db.query(queryString,(err,results,fields)=>{
+                    if(err) throw err;
+                })
+            }
+            res.send(results)
+        })
     })
 })
 router.post('/login/anak', function(req, res){
@@ -43,6 +90,9 @@ router.post('/login/anak', function(req, res){
         if(results.length > 0){
             res.send({valid:true, results:results})
             console.log(results)
+            anakSession.anak = results[0];
+            console.log(anakSession.anak)
+            console.log(anakSession)
         }
         else{
             res.send({valid:false})
@@ -51,10 +101,20 @@ router.post('/login/anak', function(req, res){
     })
 })
 router.post('/addChild', function(req, res){
-    var queryString = `INSERT INTO tbl_anak(strUserName,intMagulangId,strPassword,strFirstName,strMiddleName,strLastName)
-    VALUES(?,?,?,?,?,?)`
+    var queryString = `INSERT INTO tbl_anak(strUserName,intMagulangId,strPassword,strFirstName,strMiddleName,strLastName,intBankAccount)
+    VALUES(?,?,?,?,?,?,?)`
     console.log(magulangSession.magulang)
-    db.query(queryString,[req.body.userName,magulangSession.magulang.intUserId,req.body.passWord,req.body.firstName,req.body.middleName,req.body.lastName],(err,results,fields)=>{
+    db.query(queryString,[req.body.userName,magulangSession.magulang.intUserId,req.body.passWord,req.body.firstName,req.body.middleName,req.body.lastName,req.body.bankAccount],(err,results,fields)=>{
+        if(err) throw err;
+        console.log(results)
+        res.send(results)
+    })
+})
+router.post('/addGoal', function(req, res){
+    var queryString = `INSERT INTO tbl_goal(intAnakId,intMagulangId,dblGoal)
+    VALUES(?,?,?)`
+    console.log(magulangSession.magulang)
+    db.query(queryString,[anakSession.anak.intAnakId,anakSession.anak.intMagulangId,req.body.dblGoal],(err,results,fields)=>{
         if(err) throw err;
         console.log(results)
         res.send(results)
